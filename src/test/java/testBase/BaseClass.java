5f8b1219d315ade42bebc096a202892e99a55bce
girishkumar-repo <gbagarty94@gmail.com>
2025-06-19 08:50:48 +0530

    Updated BaseClass.java code

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -17,6 +17,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
 	
@@ -25,13 +26,20 @@ public class BaseClass {
      public Properties prop;
 	
 	@BeforeClass
-	public void setup() throws IOException
+	@Parameters({"os","browser"})
+	public void setup(String os, String br) throws IOException
 	{
 		FileReader file = new FileReader("./src//test//resources//config.properties");
 		prop= new Properties();
 		prop.load(file);
 		logger = LogManager.getLogger(this.getClass());
-		driver = new ChromeDriver();
+		switch(br.toLowerCase())
+		{
+		case "chrome":driver = new ChromeDriver(); break;
+		case "firefox":driver = new FirefoxDriver(); break;
+		case "edge":driver = new EdgeDriver(); break;
+		default: System.out.println("No Matching Browser");return;
+		}
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get(prop.getProperty("appURL"));
